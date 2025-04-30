3ae0040395bf5e5d18076b9085bbf0ef39718cf8
Im-Eradzh <eradzh.imomov@gmail.com>
2025-04-30 14:55:56 +0800

    Css

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QR Payment System</title>
-    <link rel="preload" as="style" href="{{ $apiUrl . 'build/assets/app-Bsz12FUo.css' }}">
-    <link rel="stylesheet" href="{{ $apiUrl . 'build/assets/app-Bsz12FUo.css' }}" data-navigate-track="reload">    
+    <link rel="preload" as="style" href="{{ $apiUrl . 'build/assets/app-C02hoh7K.css' }}">
+    <link rel="stylesheet" href="{{ $apiUrl . 'build/assets/app-C02hoh7K.css' }}" data-navigate-track="reload">    
 </head>
 <body class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
     <div class="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center text-center">
